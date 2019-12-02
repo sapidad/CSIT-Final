9dd7eb0ae78fbc42c2ce4b30ef1ac2bae9dcf5d6
sapidad <43483444+sapidad@users.noreply.github.com>
2019-12-02 16:46:44 -0500

    Add files via upload

--- FinalP/db.py
+++ FinalP/db.py
@@ -1,9 +1,9 @@
 import sqlite3
+import string
 from sqlite3 import Error
 from datetime import date
 from random import randint
 import re
-import string
 
 
 def estconn(db):
@@ -42,26 +42,6 @@ def c_courses(conn, cid, cname, credits, preq):
     cur.execute("INSERT OR IGNORE INTO courses(cid,cname,credits,preq) VALUES(?,?,?,?)", course)
     return cur.lastrowid
 
-def d_enroll(conn, cwid, cid):
-    cur = conn.cursor()
-    cur.execute("SELECT E.cwid, E.cid, C.cname FROM enroll E, courses C WHERE E.cid = C.cid AND E.cwid=? AND E.cid=?", (cwid, str(cid),))
-    get = cur.fetchall()
-    print(get)
-    c = str(input("Are you sure you want to withdraw from this class? Please type Y or N: "))
-    if c.lower() == 'y':
-        with conn:
-            cur.execute("DELETE FROM enroll WHERE cwid=? AND cid=?", (cwid, str(cid),))
-            cur.close()
-            print("Withdrawal Successful")
-    elif c.lower() == 'n':
-        print("Withdrawal Failed.")
-        cur.close()
-        cline(conn, cwid)
-    else:
-        print("Invalid option, please try again.")
-        cur.close()
-        d_enroll(conn, cwid, cid)
-
 def checkdb(conn, cwid):
     cur = conn.cursor()
     cur.execute("SELECT cwid FROM students WHERE cwid=?", (cwid,))
@@ -83,52 +63,55 @@ def cwidinp():
 
 def cwidgen(con):
     while True:
-        gcwid = randint(1000000, 9999999)
+        gcwid = randint(10000000, 99999999)
         cur = con.cursor()
         cur.execute("SELECT COUNT(*) FROM students WHERE cwid=? GROUP BY cwid", (gcwid,))
         exist = cur.fetchone()
         if exist is None:
-            print("cwid generated! Your cwid is: " + str(gcwid) + ".")
+            print("Your CWID is: " + str(gcwid))
             cur.close()
             return gcwid
 
-def gpainp():
+def sname():
+    name = input("Student Name: ")
+    for i in range(len(name)):
+        if name[i].isdigit():
+            print("Invalid entry, please try again.")
+            sname()
+    else:
+        return string.capwords(name)
+
+def grade():
+    g = input("Grade: ")
+    if g.lower() == 'freshman' or g.lower() == 'sophomore' or g.lower() == 'junior' or g.lower() == 'senior':
+        return string.capwords(g)
+    else:
+        print("Entries can only be Freshman, Sophomore, Junior, Senior")
+        grade()
+
+def gpa():
     while True:
         try:
-            gpa = float(input("GPA: "))
-            return gpa
+            gp = float(input("GPA: "))
+            if gp > 4.0 or gp < 0:
+                print("Invalid GPA value, please enter a valid number.")
+            else:
+                return gp
         except ValueError:
-            print("Not a valid GPA value, please try again.")
+            print("Invalid GPA value, please enter a valid number.")
 
 def n_student(con):
-    while True:
         ncwid = cwidgen(con)
-        while True:
-            nsname = str(input("Student name: "))
-            for i in range(len(nsname)):
-                if nsname[i].isdigit()==True:
-                    print("Student names cannot have numbers in them, please try again")
-                    done = False
-                    break
-                else:
-                    done = True
-            if done == True:
-                break
-                
-        while True:
-            ngrade = str(input("Grade level (please input only freshman, sophmore, junior or senior): "))
-            if (ngrade.lower() == 'freshman' or ngrade.lower() == 'sophmore' or ngrade.lower() == 'junior' or ngrade.lower() == 'senior'):
-                break
-            else:
-                print("Input must be freshman, sophmore, junior or senior.")
+        nsname = sname()
+        ngrade = grade()
+        ngpa = gpa()
 
-        ngpa = gpainp()
-        
-        print("\nYour information is:\ncwid: " + str(ncwid) + "\nname: " + string.capwords(nsname) + "\ngrade: " + ngrade.capitalize() + "\nGPA: " + str(ngpa))
+        print("\nYour Information is:\nCWID: " + str(ncwid) + "\nName: " + nsname + "\nGrade: " + ngrade + "\nGPA: " + str(ngpa))
         c = str(input("Is this correct? Please type Y or N: "))
+
         if c.lower() == 'y':
             c_student(con, ncwid, nsname, ngrade, ngpa)
-            print("You have been registered in the database! Log in to enroll in a course.")
+            print("You have been registered in the database! Log in to enroll into a course.")
             login(con)
         elif c.lower() == 'n':
             print("Enter -1 to re-enter your information.")
@@ -186,24 +169,23 @@ def login(con):
             c_courses(con, "069", "How to do the thing 101", "3", "N/A")
             c_courses(con, "777", "How to lose your money in Vegas", "3", "N/A")
 
+            c_enroll(con, "0", "21612390", "420", "4-20-2020", "A+")
+            c_enroll(con, "1", "21612390", "500", "4-01-2020", "B+")
+            c_enroll(con, "2", "21612390", "012", "3-16-2020", "F")
+            c_enroll(con, "3", "21612390", "069", "3-20-2020", "B")
 
-            c_enroll(con,"0", "21612390", "420", "4-20-2020", "A+")
-            c_enroll(con,"1", "21612390", "500", "4-01-2020", "B+")
-            c_enroll(con,"2", "21612390", "012", "3-16-2020", "F")
-            c_enroll(con,"3", "21612390", "069", "3-20-2020", "B")
+            c_enroll(con, "4", "21612391", "420", "4-20-2020", "A+")
+            c_enroll(con, "5", "21612391", "500", "4-10-2020", "B-")
+            c_enroll(con, "6", "21612391", "012", "4-15-2020", "C+")
 
-            c_enroll(con,"4", "21612391", "420", "4-20-2020", "A+")
-            c_enroll(con,"5", "21612391", "500", "4-10-2020", "B-")
-            c_enroll(con,"6", "21612391", "012", "4-15-2020", "C+")
+            c_enroll(con, "7", "21612392", "500", "1-11-2020", "A-")
+            c_enroll(con, "8", "21612392", "012", "3-22-2020", "B")
+            c_enroll(con, "9", "21612392", "069", "4-23-2020", "D")
 
-            c_enroll(con,"7", "21612392", "500", "1-11-2020", "A-")
-            c_enroll(con,"8", "21612392", "012", "3-22-2020", "B")
-            c_enroll(con,"9", "21612392", "069", "4-23-2020", "D")
+            c_enroll(con, "10", "21612393", "420", "2-03-2020", "C-")
+            c_enroll(con, "11", "21612393", "777", "2-01-2020", "D+")
 
-            c_enroll(con,"10", "21612393", "420", "2-03-2020", "C-")
-            c_enroll(con,"11", "21612393", "777", "2-01-2020", "D+")
-
-            c_enroll(con,"12", "21612394", "420", "4-20-2020", "A+")
+            c_enroll(con, "12", "21612394", "420", "4-20-2020", "A+")
         cur.close()
 
     while(existing_connection == True):
@@ -222,43 +204,49 @@ def welcome(con, cwid):
     cur = con.cursor()
     cur.execute("SELECT cwid FROM students WHERE cwid=?", (cwid,))
     getcwid = cur.fetchall()
+    cur.execute("SELECT sname FROM students WHERE cwid=?", (cwid,))
+    getname = cur.fetchall()
     s = str(getcwid)
+    n = str(getname)
     cwid = re.sub('[^A-Za-z0-9]+', '', s)
+    name = re.sub('[^A-Za-z0-9]+', ' ', n)
     print("\nWelcome, please type in a command or type commands for help!")
+    print("Student:" + name)
     print("CWID: " + cwid)
     cur.close()
     cline(con, cwid)
 
          
 def cline(con, cwid):
-    inp = str(input("Please type in a command: "))
-    if inp.isdigit() == True:
-        print("Integer values are not valid commands, try again.\n")
-    elif inp == 'commands':
-        print("L: Lists all Courses")
-        print("E: Enrolls Student(s) into a Course")
-        print("W: Withdraw Class")
-        print("S: Search for a Course")
-        print("M: View your Classes")
-        print("X: Exit")
-        print("-: Log Out\n")
-    elif inp.lower() == 'l':
-        list(con)
-    elif inp.lower() == 'e':
-        eclass(con, cwid)
-    elif inp.lower() == 'w':
-        wclass(con, cwid)
-    elif inp.lower() == 's':
-        sclass(con)
-    elif inp.lower() == 'm':
-        mclass(con, cwid)
-    elif inp.lower() == 'x':
-        exitapp(con)
-    elif inp == '-': #function i added to return to the login
-        login(con)
-    else:
-        print("Not a valid command, try again.\n")
-    cline(con, cwid)
+        inp = str(input("Please type in a command: "))
+        if inp.isdigit() == True:
+            print("Integer values are not valid commands, try again.\n")
+        elif inp == 'commands':
+            print("L: Lists all Courses")
+            print("E: Enrolls Student(s) into a Course")
+            print("W: Withdraw Class")
+            print("S: Search for a Course")
+            print("M: View your Classes")
+            print("X: Exit")
+            print("-: Log Out\n")
+        elif inp.lower() == 'l':
+            list(con)
+        elif inp.lower() == 'e':
+            eclass(con, cwid)
+        elif inp.lower() == 'w':
+            wclass(con, cwid)
+        elif inp.lower() == 's':
+            sclass(con)
+        elif inp.lower() == 'm':
+            mclass(con, cwid)
+        elif inp.lower() == 'x':
+            exitapp(con)
+        elif inp == '-':  # function i added to return to the login
+            login(con)
+        else:
+            print("Not a valid command, try again.\n")
+
+        cline(con, cwid)
 
 def list(con):
     cur = con.cursor()
@@ -269,63 +257,84 @@ def list(con):
         print(all)
 
 def eclass(conn, cwid):
-    try:
-        gcid = int(input("Please enter a Course ID: "))
-    except ValueError:
-        print("Invalid Course ID, please try again.")
-        eclass(conn, cwid)
-    cur = conn.cursor()
-    cur.execute("SELECT cid FROM courses WHERE cid=?", (gcid,))
-    records = cur.fetchall()
-    if not records:
-        print("Course does not exist, please try again.")
-        cur.close()
-        eclass(conn, cwid)
-    else:
-        cur.execute("SELECT cid, cwid FROM enroll WHERE cwid=? AND cid=?", (cwid, str(gcid)))
-        cpis = cur.fetchall()
-        cur.execute("SELECT eid FROM enroll ORDER BY eid DESC LIMIT 1;")
-        leid = str(cur.fetchall())
-        eid = int(re.sub('[^A-Za-z0-9]+', '', leid)) + 1
-        if not cpis:
-            with conn:
-                c_enroll(conn, str(eid), cwid, str(gcid), str(date.today()), "N/A")
+    while True:
+        gcid = input("Please enter a Course ID or type 'exit': ")
+        if gcid.isalpha and gcid == "exit":
+            print("Returning to main menu...\n")
+            break
+        elif gcid.isdigit():
+            cur = conn.cursor()
+            cur.execute("SELECT cid FROM courses WHERE cid=?", (gcid,))
+            records = cur.fetchall()
+            if not records:
+                print("Course does not exist, please try again.")
                 cur.close()
-                print("Enrollment Complete!")
+            else:
+                cur.execute("SELECT cid, cwid FROM enroll WHERE cwid=? AND cid=?", (cwid, str(gcid)))
+                cpis = cur.fetchall()
+                cur.execute("SELECT eid FROM enroll ORDER BY eid DESC LIMIT 1;")
+                leid = str(cur.fetchall())
+                eid = int(re.sub('[^A-Za-z0-9]+', '', leid)) + 1
+                if not cpis:
+                    cur.execute("SELECT cid, cname FROM courses WHERE cid=?", (str(gcid),))
+                    get = cur.fetchall()
+                    print(get)
+                    c = str(input("Are you sure you want to withdraw from this class? Please type Y or N: "))
+                    if c.lower() == 'y':
+                        with conn:
+                            c_enroll(conn, str(eid), cwid, str(gcid), str(date.today()), "N/A")
+                            cur.close()
+                            print("Enrollment Complete")
+                    elif c.lower == 'n':
+                        print("Enrollment Failed.")
+                        cur.close()
+                else:
+                    print("You are currently registered for this class!")
+                    cur.close()
         else:
-            print("You are currently registered for this class!")
-            cur.close()
-            eclass(conn, cwid)
+            print("Invalid Course ID, please try again.")
 
 def wclass(conn, cwid):
-    try:
-       gcid = int(input("Please enter a Course ID: "))
-    except ValueError:
-        print("Invalid Course ID, please try again.")
-        wclass(conn, cwid)
+    while True:
+       gcid = input("Please enter a Course ID or type 'exit': ")
+       if gcid.isalpha() and gcid == "exit":
+           print("Returning to main menu...\n")
+           break
+       elif gcid.isdigit() == True:
+           cur = conn.cursor()
+           cur.execute("SELECT cid FROM courses WHERE cid=?", (gcid,))
+           records = cur.fetchall()
+           if not records:
+               print("Course does not exist, please try again.")
+               cur.close()
+           else:
+               cur.execute("SELECT cid, cwid FROM enroll WHERE cwid=? AND cid=?", (cwid, str(gcid)))
+               cpis = cur.fetchall()
+               if not cpis:
+                   print("You are not registered for this class!")
+                   cur.close()
+                   wclass(conn, cwid)
+               else:
+                   with conn:
+                       cur.execute("SELECT E.cwid, E.cid, C.cname FROM enroll E, courses C WHERE E.cid = C.cid AND E.cwid=? AND E.cid=?",(cwid, str(gcid),))
+                       get = cur.fetchall()
+                       print(get)
+                       c = str(input("Are you sure you want to withdraw from this class? Please type Y or N: "))
+                       if c.lower() == 'y':
+                           with conn:
+                               cur.execute("DELETE FROM enroll WHERE cwid=? AND cid=?", (cwid, str(gcid),))
+                               cur.close()
+                               print("Withdrawal Successful")
+                       elif c.lower() == 'n':
+                           print("Withdrawal Failed.")
+                           cur.close()
+       else:
+           print("Invalid Course ID, please try again.")
 
-    cur = conn.cursor()
-    cur.execute("SELECT cid FROM courses WHERE cid=?", (gcid,))
-    records = cur.fetchall()
-    if not records:
-        print("Course does not exist, please try again.")
-        cur.close()
-        wclass(conn, cwid)
-    else:
-        cur.execute("SELECT cid, cwid FROM enroll WHERE cwid=? AND cid=?", (cwid, str(gcid)))
-        cpis = cur.fetchall()
-        if not cpis:
-            print("You are not registered for this class!")
-            cur.close()
-            wclass(conn, cwid)
-        else:
-            with conn:
-                cur.close()
-                d_enroll(conn, cwid, gcid)
 
 def sclass(con):
     while True:
-        gcname = str(input("Enter a string to search the course list for, or enter 'exit' to return to the main menu: "))
+        gcname = str(input("Enter a string to search the course list for, or type 'exit': "))
         if gcname == "exit":
             print("Returning to main menu...\n")
             break
@@ -335,16 +344,22 @@ def sclass(con):
             cur.execute("SELECT cname FROM courses WHERE cname LIKE ?", (gcname,))
             courserecord = cur.fetchall()
             cur.close()
-            for all in courserecord:
-                print(all)
+            if not courserecord:
+                print("This course does not exist in the database. Please contact your System Administrator.")
+            else:
+                for all in courserecord:
+                    print(all)
 
 def mclass(con, cwid):
     cur = con.cursor()
     cur.execute("SELECT c.cname, c.cid FROM courses c, enroll e WHERE E.cwid=? AND e.cid=c.cid", (cwid,))
     courserecord = cur.fetchall()
-    cur.close()
-    for all in courserecord:
-        print(all)
+    if not courserecord:
+        print("You are not enrolled in any classes, please enter the 'E' command to enroll in a class or 'L' to view all available classes!")
+    else:
+        for all in courserecord:
+            print(all)
+        print("")
 
 def exitapp(con):
     con.close()
